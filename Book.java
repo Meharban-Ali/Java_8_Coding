28b8878f0107c605a08c72893932c837953a35bb
Meharban-Ali <meharbanali7900@gmail.com>
2025-10-23 23:39:19 +0530

    Generate Otp feature has been created

---
+++ Book.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Book {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("This is Computer Science Book");
+        Random random = new Random();
+        String otp="";
+
+        for(int i=0; i<=5; i++){
+           int digit = random.nextInt(10);
+           otp+=digit;
+        }
+        System.out.println("Otp Generated Succesfully..");
+        System.out.println("OTP: "+otp);
+    }
+}
