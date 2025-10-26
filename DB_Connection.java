11f0855dacd69f9fd60bfe4008df56246f877169
Meharban-Ali <meharbanali7900@gmail.com>
2025-10-26 11:15:57 +0530

    Java Database Connection JDBC implementation code

---
+++ DB_Connection.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DB_Connection {
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        //url
+        String url = "jdbc:mysql://localhost:3306/product_data";
+        //username
+        String username = "root";
+        //password
+        String password = "12345";
+        // 1. Loard Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+       
+        // 2. Connection
+        Connection con = DriverManager.getConnection(url, username, password);
+        System.out.println("Database Connected Successfully..");
+
+         //Insert Query
+        String query = "insert into student(name, email, role, address) VALUES(?, ?, ?, ?)"; 
+
+        //Statement
+        PreparedStatement st = con.prepareStatement(query);
+        st.setString(1, "Rimsha");
+        st.setString(2, "rimsha123@gmail.com");
+        st.setString(3, "React Developer");
+        st.setString(4, "Mumbai");
+
+        int insertedData = st.executeUpdate();
+        
+        //Execute Query
+        // ResultSet rs = st.executeQuery();
+        if(insertedData>0) {  
+            System.out.println("Student Details Saved Successfully");
+        }
+        con.close();
+        st.close();
+    }
+}
